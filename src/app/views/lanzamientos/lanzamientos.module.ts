4cfdde85d56fe7a873d738d16de8d2fc4174da32
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-11 15:11:55 -0500

    Adicion al providers el service de spotify

--- src/app/views/lanzamientos/lanzamientos.module.ts
+++ src/app/views/lanzamientos/lanzamientos.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { LanzamientosRoutingModule } from './lanzamientos-routing.module';
 import { IndexLanzamientosComponent } from './index-lanzamientos/index-lanzamientos.component';
 import { GeneralModule } from '../../general/general.module';
+import { SpotifyService } from '../../services/spotify.service';
 
 
 @NgModule({
-  declarations: [IndexLanzamientosComponent],
-  imports: [
-    CommonModule,
-    LanzamientosRoutingModule,
-    GeneralModule
-  ]
+    declarations: [IndexLanzamientosComponent],
+    imports: [
+        CommonModule,
+        LanzamientosRoutingModule,
+        GeneralModule,
+        HttpClientModule
+    ],
+    providers: [SpotifyService]
 })
 export class LanzamientosModule { }
