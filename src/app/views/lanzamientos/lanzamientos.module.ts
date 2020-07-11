7b16dad9d045528d00dedac6a00baaf5ec077a51
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-11 08:38:13 -0500

    Importacion del generalModule en lanzamientos.module

--- src/app/views/lanzamientos/lanzamientos.module.ts
+++ src/app/views/lanzamientos/lanzamientos.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LanzamientosRoutingModule } from './lanzamientos-routing.module';
 import { IndexLanzamientosComponent } from './index-lanzamientos/index-lanzamientos.component';
+import { GeneralModule } from '../../general/general.module';
 
 
 @NgModule({
   declarations: [IndexLanzamientosComponent],
   imports: [
     CommonModule,
-    LanzamientosRoutingModule
+    LanzamientosRoutingModule,
+    GeneralModule
   ]
 })
 export class LanzamientosModule { }
