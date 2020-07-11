b6b5d4b38015e9c422a5bb5c251853bfe2a04b0c
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-10 22:49:21 -0500

    Creacion de componentes generales del proyecto

---
+++ src/app/general/components/lista-cards/lista-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-cards',
+  templateUrl: './lista-cards.component.html',
+  styleUrls: ['./lista-cards.component.scss']
+})
+export class ListaCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
