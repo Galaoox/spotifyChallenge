e638bedf25440eeae756808d9dc42b953643c454
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-11 15:36:41 -0500

    Adicion de los componentes de loading y mensaje al exports del module

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BotonesComponent } from './components/botones/botones.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoadingComponent } from './components/loading/loading.component';
+import { MensajeAlertaComponent } from './components/mensaje-alerta/mensaje-alerta.component';
 
 
 
 @NgModule({
-    declarations: [BotonesComponent],
+    declarations: [BotonesComponent, LoadingComponent, MensajeAlertaComponent],
     imports: [
         CommonModule,
         HttpClientModule
     ],
-    exports: [BotonesComponent],
+    exports: [BotonesComponent, LoadingComponent, MensajeAlertaComponent],
 
 })
 export class GeneralModule { }
