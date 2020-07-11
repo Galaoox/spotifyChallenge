641b225e7e78c7670bf76d0c7cc7c5d30cebef9d
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-11 14:37:58 -0500

    Ajuste de estilos de la grid y de las cards

--- src/app/views/lanzamientos/index-lanzamientos/index-lanzamientos.component.ts
+++ src/app/views/lanzamientos/index-lanzamientos/index-lanzamientos.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index-lanzamientos.component.scss']
 })
 export class IndexLanzamientosComponent implements OnInit {
+  datos = this.mockDatos();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+
+
+  mockDatos() {
+    return [
+      {
+        nombre: 'pruebas 1'
+      },
+      {
+        nombre: 'pruebas 2'
+      },
+      {
+        nombre: 'pruebas 3'
+      },
+      {
+        nombre: 'pruebas 4'
+      },
+      {
+        nombre: 'pruebas 6'
+      },
+      {
+        nombre: 'pruebas 6'
+      }
+    ];
   }
 
 }
