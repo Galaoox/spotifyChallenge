ec2ca3c1a9f6b6ef697a4091a69392d2aac6426a
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-10 22:27:37 -0500

    Creacion de modules para las vistas de la aplicacion - artistas , canciones , discografia, lanzamientos

---
+++ src/app/views/canciones/canciones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CancionesRoutingModule } from './canciones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CancionesRoutingModule
+  ]
+})
+export class CancionesModule { }
---
+++ src/app/views/lanzamientos/lanzamientos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LanzamientosRoutingModule } from './lanzamientos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LanzamientosRoutingModule
+  ]
+})
+export class LanzamientosModule { }
