4c91fc31fd7ead3da8a17967f5aedf185c1909d4
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 09:28:10 -0500

    Adicion del interceptor del token al module de lanzamientos

--- src/app/views/lanzamientos/lanzamientos.module.ts
+++ src/app/views/lanzamientos/lanzamientos.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { LanzamientosRoutingModule } from './lanzamientos-routing.module';
 import { IndexLanzamientosComponent } from './index-lanzamientos/index-lanzamientos.component';
 import { GeneralModule } from '../../general/general.module';
 import { SpotifyService } from '../../services/spotify.service';
+import { TokenInterceptorService } from '../../general/interceptors/token-interceptor.service';
 
 
 @NgModule({
@@ -16,6 +17,10 @@ import { SpotifyService } from '../../services/spotify.service';
         GeneralModule,
         HttpClientModule
     ],
-    providers: [SpotifyService]
+    providers: [SpotifyService, {
+        provide: HTTP_INTERCEPTORS,
+        useClass: TokenInterceptorService,
+        multi: true
+    }]
 })
 export class LanzamientosModule { }
