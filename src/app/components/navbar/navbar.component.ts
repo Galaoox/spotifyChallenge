76926d17a6861dc29f2b633ff375c556cbe328f4
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 11:24:31 -0500

    Creacion de metodo que envia a la ruta especificada el termino escrito en los buscadores

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+
+
+    buscar(termino, artista = true) {
+        const ruta = artista ? 'artistas/' : 'canciones/';
+        this.router.navigateByUrl(ruta + termino);
+    }
 
 }
