41625c196fb3cded7a521e5c5e125fe77ded326b
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 11:29:56 -0500

    Adicion del interceptor al module de artistas

--- src/app/views/artistas/artistas.module.ts
+++ src/app/views/artistas/artistas.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 import { ArtistasRoutingModule } from './artistas-routing.module';
+import { IndexArtistasComponent } from './index-artistas/index-artistas.component';
+import { GeneralModule } from '../../general/general.module';
+import { SpotifyService } from '../../services/spotify.service';
+import { TokenInterceptorService } from '../../general/interceptors/token-interceptor.service';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ArtistasRoutingModule
-  ]
+    declarations: [IndexArtistasComponent],
+    imports: [
+        CommonModule,
+        ArtistasRoutingModule,
+        GeneralModule
+    ],
+    providers: [SpotifyService, {
+        provide: HTTP_INTERCEPTORS,
+        useClass: TokenInterceptorService,
+        multi: true
+    }]
 })
 export class ArtistasModule { }
