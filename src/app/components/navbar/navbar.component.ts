3ec7a179b674a9f9eb7aa9068f0baae9a8fb7993
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 13:35:24 -0500

    Se cambia el metodo de envio del termino por el queryParams , tambien se obtienen los datos de los inputs por medio de un FormGroup

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-navbar',
@@ -8,16 +9,33 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-    constructor(private router: Router) { }
+    buscador: FormGroup;
+
+
+    constructor(private router: Router, private fb: FormBuilder) { }
 
     ngOnInit(): void {
+        this.buscador = this.crearFormulario();
+    }
+
+    crearFormulario() {
+        return this.fb.group({
+            artista: [null],
+            cancion: [null]
+        });
     }
 
 
+    buscar(artista = true) {
+        const ruta = artista ? '/artistas' : '/canciones';
+        const termino = artista ? this.buscador.value.artista : this.buscador.value.cancion;
 
-    buscar(termino, artista = true) {
-        const ruta = artista ? 'artistas/' : 'canciones/';
-        this.router.navigateByUrl(ruta + termino);
+        this.router.navigate([ruta], { queryParams: { termino: termino ? termino : '' } });
+    }
+
+    limpiarCampo(artista = true) {
+        console.log("entro a esto");
+        this.buscador.reset();
     }
 
 }
