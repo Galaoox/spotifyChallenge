4b1a8ce040833a68d50152955140603d39ef3d7e
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 11:29:17 -0500

    Ajustes de las rutas de artistas

--- src/app/views/artistas/artistas-routing.module.ts
+++ src/app/views/artistas/artistas-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexArtistasComponent } from './index-artistas/index-artistas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: IndexArtistasComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class ArtistasRoutingModule { }
