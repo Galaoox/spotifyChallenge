b6a6bc6cf51fea8c5e9417d2f988f25b2f5108fc
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 22:05:05 -0500

    Creacion de metodos para obtener las mejores canciones y la maquetacion del template

--- src/app/views/discografia/canciones-populares-discografia/canciones-populares-discografia.component.ts
+++ src/app/views/discografia/canciones-populares-discografia/canciones-populares-discografia.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SpotifyService } from '../../../services/spotify.service';
 
 @Component({
-  selector: 'app-canciones-populares-discografia',
-  templateUrl: './canciones-populares-discografia.component.html',
-  styleUrls: ['./canciones-populares-discografia.component.scss']
+    selector: 'app-canciones-populares-discografia',
+    templateUrl: './canciones-populares-discografia.component.html',
+    styleUrls: ['./canciones-populares-discografia.component.scss']
 })
 export class CancionesPopularesDiscografiaComponent implements OnInit {
+    @Input() idArtista: string;
+    canciones: any[] = [];
+    constructor(private spotifyService: SpotifyService) { }
 
-  constructor() { }
+    ngOnInit(): void {
+        this.obtenerDatos();
+    }
 
-  ngOnInit(): void {
-  }
+
+    obtenerDatos() {
+        this.spotifyService.obtenerTopCanciones(this.idArtista).subscribe(
+            (res: any) => {
+                console.log(res);
+                this.canciones = res;
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
+    convertirMsAMinutosSegundos(ms) {
+        const minutos = Math.floor(ms / 60000);
+        const segundos = Math.floor((ms - (minutos * 60000)) / 1000);
+        return `${this.concatenarCero(minutos)}:${this.concatenarCero(segundos)}`;
+    }
+
+    concatenarCero(texto) {
+        texto = String(texto);
+        console.log(texto.length);
+        return texto.length < 2 ? '0' + texto : texto;
+    }
 
 }
