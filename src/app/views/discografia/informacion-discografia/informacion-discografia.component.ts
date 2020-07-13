b2a4c57f5ddbdb8def8a5bb09923e3db72a449da
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-07-12 23:56:12 -0500

    Creacion de metodos para la obtencion de informacion y ajustes del template

--- src/app/views/discografia/informacion-discografia/informacion-discografia.component.ts
+++ src/app/views/discografia/informacion-discografia/informacion-discografia.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SpotifyService } from '../../../services/spotify.service';
 
 @Component({
-  selector: 'app-informacion-discografia',
-  templateUrl: './informacion-discografia.component.html',
-  styleUrls: ['./informacion-discografia.component.scss']
+    selector: 'app-informacion-discografia',
+    templateUrl: './informacion-discografia.component.html',
+    styleUrls: ['./informacion-discografia.component.scss']
 })
 export class InformacionDiscografiaComponent implements OnInit {
+    @Input() idArtista: string;
+    artista: any = null;
+    relacionados: any = null;
+    constructor(private spotifyService: SpotifyService) { }
 
-  constructor() { }
+    ngOnInit(): void {
+        this.obtenerDatosArtista();
+        this.obtenerDatosRelacionados();
+    }
 
-  ngOnInit(): void {
-  }
+
+    obtenerDatosArtista() {
+        this.spotifyService.obtenerInfoArtista(this.idArtista).subscribe(
+            (res: any) => {
+                this.artista = res;
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+    obtenerDatosRelacionados() {
+        this.spotifyService.obtenerArtistasRelacionados(this.idArtista).subscribe(
+            (res: any) => {
+                this.relacionados = res;
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
 
 }
